76f7d447f3407bf65d395a01b2635182a97fdc35
Joachim Wiberg <joachimpw@gmail.com>
2018-11-22 00:25:00 +0100

    a

--- ATTime/Controllers/StudentController.cs
+++ ATTime/Controllers/StudentController.cs
@@ -23,24 +23,24 @@ namespace ATTime.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult CreateStudent(Student collection)
+        public ActionResult CreateStudent(Student obj)
         {
             var context = new ATTime_DBContext();
             try
             {
                 List<object> lst = new List<object>();
-                lst.Add(collection.FirstName);
-                lst.Add(collection.LastName);
-                lst.Add(collection.Username);
-                lst.Add(collection.Psw);
+                lst.Add(obj.FirstName);
+                lst.Add(obj.LastName);
+                lst.Add(obj.Username);
+                lst.Add(obj.Psw);
                 object[] allstudents = lst.ToArray();
                 int output = context.Database.ExecuteSqlCommand("insert into students(first_name, last_name, username, psw) values(@p0,@p1,@p2,@p3)", allstudents);
                 if(output>0)
                 {
-                    ViewBag.msg = collection.FirstName + "is added";
+                    ViewBag.msg = obj.FirstName + "is added";
                 }
 
-                return RedirectToAction("Index");
+                return View();
             }
             catch
             {
