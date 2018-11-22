8f828b66eaeee2987a24b2ebabb115b406478a3c
Joachim Wiberg <joachimpw@gmail.com>
2018-11-22 02:09:32 +0100

    Godnat

---
+++ ATTime/Controllers/SearchStudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ATTime.Models;
+using System.Data.Entity;
+
+namespace ATTime.Controllers
+{
+    public class SearchStudentController : Controller
+    {
+        // GET: SearchStudent
+        public ActionResult Index()
+        {
+            var context = new ATTime_DBContext();
+
+            var student = context.Student
+                        .FromSql("select * from student")
+                        .ToList();
+
+            ViewBag.student = student;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstname)
+        {
+            ATTime_DBContext db = new ATTime_DBContext();
+            var firstnamelist = db.Student.FromSql("select * from student where first_name=@p0", firstname).ToList();
+
+            return View(firstnamelist);
+        }
+    }
+}
--- ATTime/Controllers/StudentController.cs
+++ ATTime/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ATTime.Models;
+using System.Data.Entity;
 
 namespace ATTime.Controllers
 {
@@ -22,31 +23,40 @@ namespace ATTime.Controllers
            
             return View();
         }
+
+        // SØGEFUNKTION //
+
+        [HttpPost]
+        public ActionResult Index(string firstname)
+        {
+            ATTime_DBContext db = new ATTime_DBContext();
+            var firstnamelist = db.Student.FromSql("select * from student where first_name=@p0", firstname).ToList();
+
+            return View(firstnamelist);
+        }
+                             
         [HttpPost]
         public ActionResult CreateStudent(Student obj)
         {
             var context = new ATTime_DBContext();
-            try
-            {
-                List<object> lst = new List<object>();
+            ATTime_DBContext db = new ATTime_DBContext();
+
+            List<object> lst = new List<object>();
                 lst.Add(obj.FirstName);
                 lst.Add(obj.LastName);
                 lst.Add(obj.Username);
                 lst.Add(obj.Psw);
                 object[] allstudents = lst.ToArray();
-                int output = context.Database.ExecuteSqlCommand("insert into students(first_name, last_name, username, psw) values(@p0,@p1,@p2,@p3)", allstudents);
+                int output = db.Database.ExecuteSqlCommand("insert into student(first_name, last_name, username, psw) values(@p0,@p1,@p2,@p3)", allstudents);
                 if(output>0)
                 {
                     ViewBag.msg = obj.FirstName + "is added";
                 }
 
-                return View();
-            }
-            catch
-            {
-                return View();
-            }
-        }
+                return View(output);
+            
+            
+        } 
 
-    }
+    } 
 }
