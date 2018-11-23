02ac4a18816b9cd6c6207d87791d10f98df55fd1
timx0645 <31208263+timx0645@users.noreply.github.com>
2018-11-23 14:34:32 +0100

    route

--- ATTime/Controllers/AdminViewController.cs
+++ ATTime/Controllers/AdminViewController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ATTime.Models.LoginViewModels;
+using Microsoft.EntityFrameworkCore;
+using ATTime.Models;
+using System.Data.SqlClient;
 
 namespace ATTime.Controllers
 {
@@ -10,10 +14,13 @@ namespace ATTime.Controllers
     {
         public ActionResult Index()
         {
+
             //Her tjekker vi, som vi har en session med et id i:
             if (Session["UserId"] == null)
             {
-                return View("~/Views/Login/Index");
+                //får default route
+                string routeName = ControllerContext.RouteData.Values["Default"].ToString();
+                return View(routeName);
             }
             else
             {
@@ -37,31 +44,66 @@ namespace ATTime.Controllers
                     ViewBag.start = DateTime.Now.ToString("dd/MM/yyyy");
                     ViewBag.end = DateTime.Now.AddDays(182).ToString("dd/MM/yyyy");
 
-
                     //Koden skal slutte her
                     return View();
                 }
                 else
                 {
-                    return View("~/Views/Login/Index");
+                    //får default route
+                    string routeName = ControllerContext.RouteData.Values["Default"].ToString();
+                    return View(routeName);
                 }
             }
         }
 
         public ActionResult AddCalender(DateTime start_date, DateTime end_date)
         {
+            var currentid = ((int)Session["UserId"]);
+            var currentrole = ((string)Session["UserRole"]);
+            ViewData["id"] = currentid;
+            ViewData["Role"] = currentrole;
             var today = DateTime.Now;
+            var datestring = start_date.ToString("dd/MM/yyyy");
 
-            int days_between_start = (start_date - today).Days;
-            int days_between_end = (end_date - today).Days;
+            ViewBag.msg = "Add days to your calender";
 
-
-            for (int i = days_between_start; i < days_between_end; i++)
+            if (start_date == null || end_date == null)
             {
-                DateTime.Now.AddDays(i).ToString("dd/MM/yyyy");
+                ViewBag.msg = "Add start and end date";
             }
+            else
+            { 
+                int days_between_start = (start_date - today).Days;
+                int days_between_end = (end_date - today).Days;
 
-            return View("Calender");
+                if(days_between_end < days_between_start)
+                {
+                    ViewBag.msg = "End date has to be later than Start date";
+                }
+                else
+                {
+                    var context = new ATTime_DBContext();
+                    var datecount = context.Calenders
+                           .Where(s => s.CalenderName == datestring)
+                           .Count();
+
+                    if (datecount > 0)
+                    {
+                        ViewBag.msg = "Date already in calender";
+                    }
+                    else
+                    {
+                        for (int i = days_between_start; i < days_between_end; i++)
+                        {
+                            var param = new SqlParameter("@date_calender", DateTime.Now.AddDays(i).ToString("dd/MM/yyyy"));
+                            context.Calenders.FromSql("add_calender", param);
+                            context.SaveChanges();
+                        }
+                    }
+                }
+     
+            }
+            return View("Index");
         }
 
         public ActionResult Calender()
